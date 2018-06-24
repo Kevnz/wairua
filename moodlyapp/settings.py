b5eacf9b5d214b3de9565ecc9947ee7def6f1472
Kevin Isom <kevin.isom@gmail.com>
2018-06-25 11:11:47 +1200

    log

--- moodlyapp/settings.py
+++ moodlyapp/settings.py
@@ -16,6 +16,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
